5cae9bf755095f2d169175098128c604249d856d
qiaoyansong <qiaoyansong@didiglobal.com>
2023-04-09 16:25:28 +0800

    feat init

---
+++ dal/src/main/java/com/sharding/horizontal/slicing/dal/mapper/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.sharding.horizontal.slicing.dal.mapper;
+
+import com.sharding.horizontal.slicing.dal.domain.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
